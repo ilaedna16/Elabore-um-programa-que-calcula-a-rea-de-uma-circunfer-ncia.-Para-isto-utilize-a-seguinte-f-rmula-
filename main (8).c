1b091ac789fdf40c875372b1585f77f8beec29b3
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-16 20:29:12 -0300

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  
+ int area; 
+ 
+ printf ("informe a area de uma circunferencia\n"); 
+ scanf ("%d",&area); 
+ 
+ area =(area *area);
+ 
+ printf ("a area da circunferencia eh:%.d\n", area); 
+  
+  
+  
+    return 0;
+}
